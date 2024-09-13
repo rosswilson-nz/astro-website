a256b750e5531715035bef7a09fcbafc95d53809
Ross Wilson <ross.wilson@otago.ac.nz>
2024-09-13 22:14:20 +0200

    complete tutorial

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,8 @@
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+import preact from "@astrojs/preact";
+
+export default defineConfig({
+  site: "https://calm-hotteok-23d102.netlify.app/",
+  integrations: [preact()]
+});
